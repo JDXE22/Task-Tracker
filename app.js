666a363fa5254ebcc32fd6211a74a07fb5b49835
David <davidesparzaj22@gmail.com>
2025-06-05 21:16:06 -0500

    chore:
    initialize Task Tracker CLI with app.js
    
    and tasks.json

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv/config";
+import tasks from "./tasks.json" assert { type: "json" };
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+
+const command = process.argv[2];
+if (command === "start") {
+  console.log("Starting the application...");
+} 
+
+app.get("/tasks", (req, res, next) => {
+  try {
+    res.status(200).json(tasks);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
