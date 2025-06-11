5fa85a18502eb4ebc4c9dbccd99e6c79139f3ea6
David <davidesparzaj22@gmail.com>
2025-06-10 20:26:28 -0500

    feat(src/utils/services/tasksServices.js):
    implement addTask function to add new tasks
    
    and return confirmation

--- src/utils/services/tasksServices.js
+++ src/utils/services/tasksServices.js
@@ -4,4 +4,14 @@ export const getTasks = () => {
   tasks.forEach((task) => {
     console.log(`- ${task.name}: ${task.description}`);
   });
+
+  // for (const task of tasks) {
+  // console.log(`- ${task.name}: ${task.description}`);}
+  
 };
+
+export const addTask = (task) => {
+
+  tasks.push(task)
+  return {message: "Task added successfully", ID: task.id};
+}
