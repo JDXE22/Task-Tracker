ba9a7dc987f2714610d4d7e6cf00013cf5b0f353
David <davidesparzaj22@gmail.com>
2025-06-12 14:29:01 -0500

    refactor(src/utils/commands/index.js):
    dynamically generate mark commands for task
    
    statuses in commands export

--- src/dispatch.js
+++ src/dispatch.js
@@ -2,6 +2,7 @@ import { commands } from "./utils/commands/index.js";
 import { unknown } from "./utils/middlewares/unknownCommand/unknown.js";
 
 export const dispatch = ([cmd, ...args]) => {
-  const command = commands[cmd] ?? (() => unknown({ name: cmd }));
+  const command = commands[cmd] ?? unknown(cmd);
+  
   return command(...args);
 };
--- src/utils/commands/index.js
+++ src/utils/commands/index.js
@@ -1,9 +1,19 @@
 import { addTask, listCommands, markTaskAs, removeTask, updateTask } from "./commandsControllers.js";
 
+const statuses = [ "todo", "in-progress", "done" ];
+
+
+const markCommands = Object.fromEntries(
+  statuses.map((status) => [
+    `mark-${status}`,
+    (id) => markTaskAs(id, status),
+  ])
+)
+
 export const commands = {
   list: listCommands,
   add: addTask,
   delete: removeTask,
   update: updateTask,
-  mark: markTaskAs,
+  ...markCommands
 };
