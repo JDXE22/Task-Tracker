69f21f4de52d48f631c79c507ad4a208b65e8a90
David <davidesparzaj22@gmail.com>
2025-06-12 11:42:21 -0500

    refactor(src/utils/commands/
    commandsControllers.js):
    
    move all command functions to commandsController
    
    file

---
+++ src/utils/commands/commandsControllers.js
@@ -0,0 +1,41 @@
+import { errorMiddleware } from "../middlewares/error/errors.js";
+import {
+  deleteTaskService,
+  updateTaskService,
+} from "../services/tasksServices.js";
+import { getTasks } from "../services/tasksServices.js";
+import { addTaskService } from "../services/tasksServices";
+
+export const listCommands = () => {
+  try {
+    getTasks();
+  } catch (error) {
+    errorMiddleware(error);
+  }
+};
+
+export const removeTask = (id) => {
+  try {
+    deleteTaskService(id);
+  } catch (error) {
+    errorMiddleware(error);
+  }
+};
+
+export const addTask = (task) => {
+  try {
+    const saved = addTaskService(task);
+    return saved;
+  } catch (error) {
+    errorMiddleware(error);
+  }
+};
+
+export const updateTask = (id, task) => {
+  try {
+    const updatedTask = updateTaskService(id, task);
+    return updatedTask;
+  } catch (error) {
+    errorMiddleware(error);
+  }
+};
